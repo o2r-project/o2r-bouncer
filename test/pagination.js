65bf5324f9939d3322053cf402c4fb8d1b2def7f
nuest <daniel.nuest@uni-muenster.de>
2017-10-27 14:24:46 +0200

    reactivate all tests

--- test/pagination.js
+++ test/pagination.js
@@ -28,7 +28,7 @@ const cookie_plain = 's:yleQfdYnkh-sbj9Ez--_TWHVhXeXNEgq.qRmINNdkRuJ+iHGg5woRa9y
 const cookie_admin = 's:hJRjapOTVCEvlMYCb8BXovAOi2PEOC4i.IEPb0lmtGojn2cVk2edRuomIEanX6Ddz87egE5Pe8UM';
 const cookie_editor = 's:xWHihqZq6jEAObwbfowO5IwdnBxohM7z.VxqsRC5A1VqJVspChcxVPuzEKtRE+aKLF8k3nvCcZ8g';
 
-describe.only('User list pagination', () => {
+describe('User list pagination', () => {
     describe('illegal pagination parameters', () => {
         
         it('should response with HTTP 400 and valid JSON error when start is too small', (done) => {
